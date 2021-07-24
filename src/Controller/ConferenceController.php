232b4e7b2fc3c3c86fa1c5b9809f5f6b69e6eafb
joshua530 <joshuaomari5@gmail.com>
2021-07-24 18:02:04 -0400

    Modify default home page appearance

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,31 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/{name}", name="homepage")
      */
-    public function index(string $name = ''): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository,): Response
     {
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        return new Response(<<<EOF
-        <!doctype html>
-        <html>
-            <head></head>
-            <body>
-                $greet
-                <img src="/images/under-construction.gif" />
-            </body>
-        </html>
-    EOF);
+        return new Response(
+            $twig->render(
+                'conference/index.html.twig',
+                ['conferences' => $conferenceRepository->findAll()]
+            ),
+        );
     }
 }
