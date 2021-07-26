84b141facb9d7655688d0fd2d055657b2e8bd2fa
joshua530 <joshuaomari5@gmail.com>
2021-07-26 16:08:31 -0400

    Specify slug field as unique

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,9 +6,12 @@ use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
+ * @UniqueEntity("slug")
  */
 class Conference
 {
@@ -40,7 +43,7 @@ class Conference
     private $comments;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255, nullable=false, unique=true)
      */
     private $slug;
 
@@ -52,6 +55,13 @@ class Conference
     public function __toString(): string
     {
         return $this->city . ' ' . $this->year;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
     }
 
     public function getId(): ?int
