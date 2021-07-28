3a0c44adb9907f5d03c0765eb1b47701f64d17fd
joshua530 <joshuaomari5@gmail.com>
2021-07-28 15:18:36 +0300

    Add more tests

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,36 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
+
+    // failing
+    // public function testCommentSubmission()
+    // {
+    //     $client = static::createClient();
+    //     $crawler = $client->request('GET', '/conference/Amsterdam-2019');
+
+    //     $client->submitForm('comment_form_submit', [
+    //         'comment_form[author]' => 'john',
+    //         'comment_form[text]' => 'some feedback from an automated functional test',
+    //         'comment_form[email]' => 'john@autotest.org',
+    //         'comment_form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif'
+    //     ]);
+    //     $this->assertResponseRedirects();
+    //     $client->followRedirect();
+    //     $this->assertSelectorExists('div:contains("There are 2 comments")');
+    // }
 }
